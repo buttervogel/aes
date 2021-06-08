776fa9c03b5da6a1ccf59653e8e8ddd8366b1559
oliver.meimberg <oliver.meimberg@form4.de>
2021-06-09 00:13:40 +0200

    Finished

--- src/AESCrypt.java
+++ src/AESCrypt.java
@@ -1,59 +1,33 @@
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 import java.util.Base64;
 
 public class AESCrypt {
 
-    private static String ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String ALGORITHM = "AES";
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
 
-
-    protected static SecretKey getKeyFromPassword(String password, String salt) throws Exception {
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
-        return secret;
+    public static String encrypt(String valueToEnc, String password) throws Exception {
+        Key key = generateKey(password);
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encValue = c.doFinal(valueToEnc.getBytes());
+        return new String(Base64.getEncoder().encode(encValue));
     }
 
-    public static IvParameterSpec generateIv() {
-        byte[] iv = new byte[16];
-        new SecureRandom().nextBytes(iv);
-        return new IvParameterSpec(iv);
+    public static String decrypt(String encryptedValue, String password) throws Exception {
+        Key key = generateKey(password);
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = Base64.getDecoder().decode(encryptedValue);
+        byte[] decValue = c.doFinal(decordedValue);
+        return new String(decValue);
     }
 
-    protected static String encrypt(String algorithm, String input, SecretKey key, IvParameterSpec iv) throws Exception {
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-        byte[] cipherText = cipher.doFinal(input.getBytes());
-        return Base64.getEncoder().encodeToString(cipherText);
-    }
-
-    protected static String decrypt(String algorithm, String cipherText, SecretKey key, IvParameterSpec iv) throws Exception {
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
-        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
-        return new String(plainText);
-    }
-
-    private static SecretKey generateKey(String password) throws Exception {
-        Key key = new Key
-         return getKeyFromPassword(password, "pepper");
-    }
-
-    public static String encrypt(String input, String password) throws Exception {
-        return encrypt(ALGORITHM, input, generateKey(password), AESCrypt.generateIv());
-    }
-
-    public static String decrypt(String input, String password) throws Exception {
-        return decrypt(ALGORITHM, input, generateKey(password), AESCrypt.generateIv());
+    private static Key generateKey(String password) {
+        return new SecretKeySpec(password.getBytes(CHARSET), ALGORITHM);
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,34 +1,22 @@
 import org.junit.Test;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-
 public class Tester {
 
+    public static final String INPUT = "oliver.meimberg";
+    public static final String ENC_INPUT = "tzVZTKv/il4mh8cBvGghHg==";
+    public static final String PASSWORD = "Oliedvf..o345ru9";
+
     @Test
-    public void givenString_whenEncrypt_thenSuccess() throws Exception {
-        String input = "my.very.secret";
-        SecretKey key = AESCrypt.getKeyFromPassword("dfvinjmo","pepper");
-        IvParameterSpec ivParameterSpec = AESCrypt.generateIv();
-        String algorithm = "AES/CBC/PKCS5Padding";
-        String cipherText = AESCrypt.encrypt(algorithm, input, key, ivParameterSpec);
-        String plainText = AESCrypt.decrypt(algorithm, cipherText, key, ivParameterSpec);
-        assert input.equals(plainText);
+    public void testShorthand() throws Exception {
+        String encryptedInput = AESCrypt.encrypt(INPUT, PASSWORD);
+        String decryptedInput = AESCrypt.decrypt(encryptedInput, PASSWORD);
+        assert INPUT.equals(decryptedInput);
     }
 
     @Test
-    public void testShorthand() throws  Exception {
-        String input = "my.very.secret2";
-        String password = "pups";
-
-
-        String encryptedInput = AESCrypt.encrypt(input,password);
-        String decryptedInput = AESCrypt.decrypt(encryptedInput,password);
-
-        assert input.equals(decryptedInput);
-
-
-
+    public void testRecover() throws Exception {
+        String decryptedInput = AESCrypt.decrypt(ENC_INPUT, PASSWORD);
+        assert INPUT.equals(decryptedInput);
     }
 
 }
